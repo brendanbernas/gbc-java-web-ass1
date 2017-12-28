6b85b308fe13949120462bfcdea33ea418c9108b
brendanbernas <brendan.bernas@georgebrown.ca>
2017-12-28 10:45:40 -0500

    Updated DB, Added ERD

--- PEGASUS_DB.sql
+++ PEGASUS_DB.sql
@@ -4,113 +4,114 @@ CREATE DATABASE IF NOT EXISTS COMP3095;
 USE COMP3095;
 grant all on COMP3095.* to 'admin'@'localhost' identified by 'admin'; 
 
-CREATE TABLE USERS 
+CREATE TABLE admin
 ( 
 	id int(11) AUTO_INCREMENT PRIMARY KEY, 
-	firstname varchar(255),
-	lastname varchar(255),
+	first_name varchar(255),
+	last_name varchar(255),
 	email varchar(255), 
 	role varchar(20),
-	username varchar(20) UNIQUE,
-	password varchar(20)	
+	username varchar(20) UNIQUE NOT NULL,
+	password varchar(20) NOT NULL	
 );
 
-CREATE TABLE Logged_In_T
+CREATE TABLE logged_in
 (
-	id char(36),
-    UserId int(11),
-    PRIMARY KEY(id),
-    FOREIGN KEY (UserId) REFERENCES USERS(id)
+	id char(36) PRIMARY KEY,
+    admin_id int(11) NOT NULL,
+    FOREIGN KEY (admin_id) REFERENCES admin(id)
 );
 
-INSERT INTO USERS (firstname, lastname, email, role, username, password) VALUES
+INSERT INTO admin (first_name, last_name, email, role, username, password) VALUES
 	('Admin', 'Adams', 'aadams@gbc.ca', NULL, 'admin', 'admin');
 
-CREATE TABLE Employee_T
+CREATE TABLE department
 (
-	EmployeeID int(9) AUTO_INCREMENT PRIMARY KEY,
-	EmployeeFirst varchar(255),
-	EmployeeLast varchar(255),
-	EmployeeEmail varchar(255),
-	DateHired DateTime,
-	EmployeePosition varchar(255)
+	id int(2) AUTO_INCREMENT PRIMARY KEY,
+	name varchar(255) UNIQUE NOT NULL,
+	location varchar(255) NOT NULL
 );
 
-CREATE TABLE Department_T
+CREATE TABLE employee
 (
-	DepartmentID int(2) AUTO_INCREMENT PRIMARY KEY,
-	DepartmentName varchar(255) UNIQUE,
-	DepartmentLocation varchar(255)
+	id int(9) AUTO_INCREMENT PRIMARY KEY,
+    department_id int(2) NOT NULL,
+	first_name varchar(255) NOT NULL,
+	last_name varchar(255) NOT NULL,
+	email varchar(255) UNIQUE NOT NULL,
+	date_hired DateTime NOT NULL,
+	position varchar(255) NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE Group_T
+CREATE TABLE groups
 (
-	GroupID int(4) AUTO_INCREMENT PRIMARY KEY,
-	GroupName varchar(255) UNIQUE,
-	GroupDepartmentID int(2),
-	FOREIGN KEY (GroupDepartmentID) REFERENCES Department_T(DepartmentID)
+	id int(4) AUTO_INCREMENT PRIMARY KEY,
+	department_id int(2) NOT NULL,
+	name varchar(255) UNIQUE NOT NULL,
+	FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE GroupMembers_T
+CREATE TABLE group_member
 (
-	GroupID int(9),
-	EmployeeID int(9),
-	FOREIGN KEY (GroupID) REFERENCES Group_T(GroupID),
-	FOREIGN KEY (EmployeeID) REFERENCES Employee_T(EmployeeID)		
+	group_id int(9) NOT NULL,
+	employee_id int(9) NOT NULL,
+	FOREIGN KEY (group_id) REFERENCES groups(id),
+	FOREIGN KEY (employee_id) REFERENCES employee(id)		
 );
 
 CREATE TABLE report_template
 (
 	id int(5) AUTO_INCREMENT PRIMARY KEY,
-    department_id int(2),
-    mapping_id int(9),
-    template_type tinyint(1),
-    template_name varchar(255),
-    FOREIGN KEY (department_id) REFERENCES Department_T(DepartmentID)
+    department_id int(2) NOT NULL,
+    mapping_id int(9) NOT NULL,
+    template_type tinyint(1) NOT NULL,
+    template_name varchar(255) NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE report_section_template
 (
 	id int(8) AUTO_INCREMENT PRIMARY KEY,
-	report_template_id int(5),
-    template_name varchar(255),
-    section_number tinyint(1),
+	report_template_id int(5) NOT NULL,
+    template_name varchar(255) NOT NULL,
+    section_number tinyint(1) NOT NULL,
     FOREIGN KEY (report_template_id) REFERENCES report_template(id)
 );
 
 CREATE TABLE report_criteria_template
 (
 	id int(11) AUTO_INCREMENT PRIMARY KEY,
-    report_section_template_id int(8),
-    template_name varchar(255),
-    criteria_number tinyint(1),
-    max_evaluation tinyint(1),
+    report_section_template_id int(8) NOT NULL,
+    template_name varchar(255) NOT NULL,
+    criteria_number tinyint(1) NOT NULL,
+    max_evaluation tinyint(1) NOT NULL,
     FOREIGN KEY (report_section_template_id) REFERENCES report_section_template(id)
 );
 
 CREATE TABLE report
 (
 	id int(11) AUTO_INCREMENT PRIMARY KEY,
-    report_template_id int(5),
-    report_name varchar(255),
-    report_date datetime,
+    report_template_id int(5) NOT NULL,
+    report_name varchar(255) NOT NULL,
+    report_date datetime NOT NULL,
     FOREIGN KEY (report_template_id) REFERENCES report_template(id)
 );
 
 CREATE TABLE evaluation
 (
 	id int(11) AUTO_INCREMENT PRIMARY KEY,
-    report_id int(11),
-    report_criteria_template_id int(11),
-    grade int(3),
+    report_id int(11) NOT NULL,
+    report_criteria_template_id int(11) NOT NULL,
+    grade int(3) NOT NULL,
     evaluation_comment varchar(255),
     FOREIGN KEY (report_id) REFERENCES report(id),
     FOREIGN KEY (report_criteria_template_id) REFERENCES report_criteria_template(id)
 );
 
 #Dummy data
-INSERT INTO Employee_T (EmployeeFirst, EmployeeLast, EmployeeEmail, DateHired, EmployeePosition)
-  VALUES ('Brendan', 'Bernas', 'brendan.bernas@georgebrown.ca', '2017/10/17', 'Manager');
-
-INSERT INTO Department_T(DepartmentName, DepartmentLocation) 
+INSERT INTO department(name, location) 
   VALUES ('Human Resources', 'Casa Loma');
+
+INSERT INTO employee(first_name, last_name, email, date_hired, position, department_id)
+  VALUES ('Brendan', 'Bernas', 'brendan.bernas@georgebrown.ca', '2017/10/17', 'Manager', 1);
